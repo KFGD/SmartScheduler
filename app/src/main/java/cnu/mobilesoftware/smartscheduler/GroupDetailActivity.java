0c7091492c6792210f6bb7665131c1efaee7b0b7
GwanYong Kim <kky960105@naver.com>
2016-11-28 18:15:56 +0900

    일해라 염철민!
    일해라 염철민!

--- app/src/main/java/cnu/mobilesoftware/smartscheduler/GroupDetailActivity.java
+++ app/src/main/java/cnu/mobilesoftware/smartscheduler/GroupDetailActivity.java
@@ -3,16 +3,26 @@ package cnu.mobilesoftware.smartscheduler;
 import android.content.Intent;
 import android.os.Build;
 import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.transition.Explode;
 import android.transition.Transition;
-import android.util.Log;
 import android.view.Menu;
-import android.view.View;
 import android.view.Window;
 import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cnu.mobilesoftware.smartscheduler.Fragment.NoticeFragment;
+import cnu.mobilesoftware.smartscheduler.Fragment.PostFragment;
+import cnu.mobilesoftware.smartscheduler.Interface.ITitle;
 
 public class GroupDetailActivity extends AppCompatActivity {
 
@@ -50,17 +60,61 @@ public class GroupDetailActivity extends AppCompatActivity {
         final Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
         CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout)findViewById(R.id.collapsing_toolbar);
-
         collapsingToolbarLayout.setTitle(groupItem.group_title);
-
         memberImage.setImageResource(groupItem.img_res);
+
+
+        //ViewPager
+        ViewPager viewPager = (ViewPager)findViewById(R.id.viewPager);
+        TabLayout tabLayout = (TabLayout)findViewById(R.id.tabLayout);
+        setUpViewPagerAndTabLayout(viewPager, tabLayout);
+    }
+
+    private void setUpViewPagerAndTabLayout(ViewPager viewPager, TabLayout tabLayout){
+        //Setting ViewPager
+        SectionsPagerAdapter sectionPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        sectionPagerAdapter.appendFragment(NoticeFragment.newInstance());
+        sectionPagerAdapter.appendFragment(PostFragment.newInstance());;
+        viewPager.setAdapter(sectionPagerAdapter);
+        viewPager.setCurrentItem(0);
+
+        //Setting TabLayout
+        tabLayout.setupWithViewPager(viewPager);
+        for(int i=0; i<sectionPagerAdapter.getCount(); ++i){
+            if(null == tabLayout.getTabAt(i))
+                break;
+            if(sectionPagerAdapter.getItem(i) instanceof ITitle)
+                tabLayout.getTabAt(i).setText(((ITitle) sectionPagerAdapter.getItem(i)).getTitle());
+        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         //getMenuInflater().inflate(R.menu.sample_actions, menu);
         return true;
+    }
+
+    public final class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        private final List<Fragment> pageFragment = new ArrayList<>(3);
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        public void appendFragment(Fragment fragment){
+            pageFragment.add(fragment);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return pageFragment.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return pageFragment.size();
+        }
     }
 }
